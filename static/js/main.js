5eb382bb5811bb9922d308640bb50b84060a4648
L1224rd <edermarques96@gmail.com>
2018-06-10 15:49:13 -0300

    Correção do mask do telefone, script do carrosel no main.js e ajuste de tags no head

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#id_telefone').mask('(00) 0 0000-0000');
 
     $('option:first').attr("disabled", "disabled");
+    
+    $('#carrossel').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: true,
+        focusOnSelect: false,
+        dotsClass: 'slick-dots dots-css',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
 });
