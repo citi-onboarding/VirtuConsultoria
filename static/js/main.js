ca201c2d296bfd90cf75661469837702ba94637d
Alecsandro <ascj@cin.ufpe.br>
2018-06-12 14:13:22 -0300

    Script do maps no main.js

--- static/js/main.js
+++ static/js/main.js
@@ -174,3 +174,16 @@ function validarSheet(ordemServicos) {
 function website(link){
     window.open(link);
 }
+
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+    });
+    var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+    });
+}
+
