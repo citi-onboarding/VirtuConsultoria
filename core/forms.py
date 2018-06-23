2f78e11cd30f359e33a819a01b416a8fa8b3d6e6
L1224rd <edermarques96@gmail.com>
2018-06-22 21:10:59 -0300

    teste de alinhamento com textarea

--- core/forms.py
+++ core/forms.py
@@ -15,4 +15,4 @@ class emailForm(forms.Form):
     email = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Email'}), max_length=60)
     assunto = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Assunto'}), max_length=200)
     conheceu = forms.ChoiceField(label="", choices=comoConheceuOpcoes)
-    mensagem = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Mensagem'}), max_length=2000)
+    mensagem = forms.CharField(label="", widget=forms.Textarea(attrs={'width':"100%", 'cols' : "80", 'rows': "8", 'placeholder': ' Mensagem'}))
