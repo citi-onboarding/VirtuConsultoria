7ddda9ec620591a5434cc267bca092d89ba0b6c6
unknown <ahtlc@cin.ufpe.br>
2018-06-23 14:25:36 -0300

    Depoimentos pronto

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,11 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-<<<<<<< HEAD
     #'whitenoise.middleware.WhiteNoiseMiddleware',
-=======
-    'whitenoise.middleware.WhiteNoiseMiddleware',
->>>>>>> teste
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
