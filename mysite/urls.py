7339fe763d76fdf38737739268676fd04333e2c7
Alecsandro <ascj@cin.ufpe.br>
2018-06-26 21:53:03 -0300

    adashda

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from core.views import *
 
+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
